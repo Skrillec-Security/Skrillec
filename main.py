bc0e15624bef8261b3b84078e63a2f2b952aa965
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-15 12:12:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os, sys, time, threading, discord
-from Bot.main import *
+from src.Bot.main import *
+from src.Config.main import *
 
 bot = Skrillec()
 threading.Thread(target=bot.run, args=('OTA5NTAwOTkwMTM3NDQyNDE2.YZFMzQ.MGAdb6rsyU1OOGFrOAgIYLKCi_M',)).start()
