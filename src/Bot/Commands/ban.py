67c57978924613dd2e5c13f9186c6b8d7090f5dd
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-15 12:50:50 -0500

    Create ban.py

---
+++ src/Bot/Commands/ban.py
@@ -0,0 +1,8 @@
+import discord
+
+"""
+Banner System used for banning and unbanning users
+Note: Use 0 for ban and 1 to unban a user on 'bantoggle' argument value
+"""
+async def Banner_Syetem(client, userid, bantoggle: int):
+    await client.channel.send("test")
