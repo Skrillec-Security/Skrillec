4094c73e2d4f470c71de7504b400bd5531dd8209
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-16 12:27:02 -0500

    Update clear.py

--- src/Bot/Commands/clear.py
+++ src/Bot/Commands/clear.py
@@ -1,3 +1,9 @@
 import os, sys, time, discord
 
-def Clear_System(client, fmsg, msg_args):
+# def Clear_System(client, fmsg, msg_args):
+#     if client.message.author.guild_permissions.administrator or client.author.id == "":
+#         if len(msg_args) < 1:
+#             ## nuke the channel
+#         else:
+#             await 
+
