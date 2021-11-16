3136c218ac7e86f2071a037ca33d9317c630cbe1
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-16 10:08:59 -0500

    Update ban.py

--- src/Bot/Commands/ban.py
+++ src/Bot/Commands/ban.py
@@ -1,8 +1,26 @@
 import discord
 
 """
-Banner System used for banning and unbanning users
-Note: Use 0 for ban and 1 to unban a user on 'bantoggle' argument value
+Advanced Banning System With Unbanning
 """
-async def Banner_Syetem(client, userid, bantoggle: int):
-    await client.channel.send("test")
+async def Ban_Syetem(client, fmsg, msg_args):
+    if client.message.author.guild_permissions.administrator or client.author.id == "909116206668214282":
+        reason = "" ## Empty as no reason for ban
+        if len(msg_args) > 1:
+            userid = msg_args[1]
+            if "<@" in userid: userid = userid.replace("<@", "").replace(">","").replace("!", "")
+            userid = int(userid)
+            if len(msg_args) > 2:
+                if msg_args[2].startswith("reason="): 
+                    reason = msg_args[2].replace("reason=", "")
+                    reason += "\nBanned By Skrillec Bot!"
+                    await client.channel.send("Banning <@{}>!\nRequested By: <@{}>".format(userid, client.author.id))
+                    await client.guild.ban(discord.Object(id=userid), reason=reason)
+                elif msg_args[2] == "1":
+                    await client.channel.send("Unbanning <@{}>!\nRequested By <@{}>".format(userid, client.author.id))
+                    await client.guild.unban(discord.Object(id=userid))
+            else:
+                await client.channel.send("Banning <@{}>!\nRequested By: <@{}>".format(userid, client.author.id))
+                await client.guild.ban(discord.Object(id=userid), reason=reason)
+        else:
+            await client.channel.send("```[x] Invalid argument!\nUsage: >ban <userid> <reason/1> (use 1 for reason to unban user)\nExample: >ban @skrillec```")
