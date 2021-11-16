edff0092b4969b909ad141eef4a815d26bd96c17
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-16 12:27:18 -0500

    Update main.py

--- src/Config/main.py
+++ src/Config/main.py
@@ -1,10 +1,22 @@
 
 class Config:
-    Help_CMDs = r""">Help Moderation | List Of Moderation Commands
+
+    Bot_Info = {
+        "Title": "Skrillec",
+        "Description": "The 2022 New All-In-One Moderation. Eliminating the usage of multiple bots with limited commands on each",
+        "Version": "1.00"
+    }
+
+    Help_CMDs = r"""List Of Commands
+________________________________________________
+>Help Moderation | List Of Moderation Commands
 >Help IPTools | List Of IP/Networking Tools
 >Help ASCII | List of ASCII/ANSI Convertion Tools
->Help Admin | List of Skrillec admin commands
-    """
+>Help Admin | List of Skrillec admin commands"""
 
-    Moderation_CMDs = r""">nuke
-    """
+    Moderation_CMDs = r"""List Of Moderation Commands
+_______________________________________
+>clear <amount> | Clear Messages
+>ban <user> <reason> | Ban A User"""
+
+    ansi_CMDs = r""" """
