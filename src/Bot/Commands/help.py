5bfa963de8be84564496400a75e989791fc8794a
WhosGotFrost <whosgothoes@gmail.com>
2021-11-16 21:21:36 -0500

    Update help.py

--- src/Bot/Commands/help.py
+++ src/Bot/Commands/help.py
@@ -6,7 +6,8 @@ async def Help(client, fmsg, msg_args):
     if len(msg_args) > 1:
         if msg_args[1] == "mod" or msg_args[1] == "moderation":
             ## show moderation list
-            await client.channel.send("```{}```".format(Config.Moderation_CMDs))
+            # await client.channel.send("```{}```".format(Config.Moderation_CMDs))
+            await (Config.embed('Moderation List', '```{}```'.format(Config.Moderation_CMDs), client))
         elif msg_args[1] == "iptools":
             ## show iptools list
             await client.channel.send("g")
@@ -14,4 +15,4 @@ async def Help(client, fmsg, msg_args):
             await client.channel.send("")
         elif msg_args[1] == "admin":
             # await client.channel.send("")
-            await Config.embed('Test', 'Just testing this function and seeing if it works!', client)
+            await Config.embed('Admin', '***ADMIN LIST HERE***', client)
