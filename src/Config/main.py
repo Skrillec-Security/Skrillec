4db825afbf433a1c1c9e8041dffabb908b63db39
WhosGotFrost <whosgothoes@gmail.com>
2021-11-16 21:12:06 -0500

    Update main.py

--- src/Config/main.py
+++ src/Config/main.py
@@ -30,5 +30,5 @@ _______________________________________
     ansi_CMDs = r""" """
 
     async def embed(name, description, client):
-        embed =discord.Embed(title=name, description=description, color=discord.Color.red())
+        embed=discord.Embed(title=name, description=description, color=discord.Color.red())
         await client.channel.send(embed=embed)
