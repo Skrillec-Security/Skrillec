d44a9ad9306babab9d025ed2132fe8f09f665327
WhosGotFrost <whosgothoes@gmail.com>
2021-11-21 15:06:11 -0500

    Update kick.py

--- src/Bot/Commands/kick.py
+++ src/Bot/Commands/kick.py
@@ -9,6 +9,7 @@ async def dick(client, fmsg, msg_args, reason='Bad intentions'):
         if len(msg_args) > 1:
             userid = msg_args[1]
             if "<@" in userid: userid = userid.replace("<@", "").replace(">","").replace("!", "")
+            elif userid == '':  await client.channel.send('missing arguments.') 
             userid = int(userid)
             await client.guild.kick(discord.Object(id=userid))
             kick = discord.Embed(title='User has been Kicked!', color=discord.Color.dark_purple(), description='<@{}> has been kicked'.format(userid))
