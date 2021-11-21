efc594b60a1919996944e8069edbe5a5c319a240
WhosGotFrost <whosgothoes@gmail.com>
2021-11-21 14:32:24 -0500

    Create kick.py

---
+++ src/Bot/Commands/kick.py
@@ -0,0 +1,15 @@
+import discord
+
+
+async def dick(client, fmsg, msg_args, reason='Bad intentions'):
+    if client.author.guild_permissions.administrator == False:
+        await client.channel.send('not enough perms.')
+        return
+    if client.author.guild_permissions.administrator:
+        if len(msg_args) > 1:
+            userid = msg_args[1]
+            if "<@" in userid: userid = userid.replace("<@", "").replace(">","").replace("!", "")
+            userid = int(userid)
+            await client.guild.kick(discord.Object(id=userid))
+            kick = discord.Embed(title='User has been Kicked!', color=discord.Color.dark_purple(), description='<@{}> has been kicked'.format(userid))
+            await client.channel.send(embed=kick)
