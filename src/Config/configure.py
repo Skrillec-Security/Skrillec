7617f14d7d2afc9c7ae62edb5f3609880583eeec
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-11-20 19:53:29 -0500

    Update configure.py

--- src/Config/configure.py
+++ src/Config/configure.py
@@ -5,6 +5,7 @@ from .main import *
 class Configuration:
     async def Configure_MSG_Info(client):
         Config.Current["fullmsg"] = client.content
+        Config.Current['args'] = ["", ""]
         if " " in client.content:
             Config.Current['args'] = (client.content).split(" ")
             Config.Current["cmd"] = Config.Current['args'][0].replace(Config.Bot_Info['Prefix'], "")
