31a318b8a1345d1d10f139f01147c00378cda19f
WhosGotFrost <whosgothoes@gmail.com>
2021-11-21 16:14:13 -0500

    Update clear.py

--- src/Bot/Commands/clear.py
+++ src/Bot/Commands/clear.py
@@ -1,4 +1,6 @@
+from logging import error
 import os, sys, time, discord
+from ..discord_utils import *
 
 # def Clear_System(client, fmsg, msg_args):
 #     if client.message.author.guild_permissions.administrator or client.author.id == "":
@@ -7,3 +9,19 @@ import os, sys, time, discord
 #         else:
 #             await 
 
+
+async def Clear_System(client, fmsg, msg_args):
+    try:
+        admin = client.author.guild_permissions.administrator;
+
+        if admin == False: await discordUtils.error_Message(client, '***You do not have the required permissions to use this command!***') ; return
+
+        elif admin:
+            if len(msg_args) > 1:
+                amount = msg_args[1]
+                if amount == '': await discordUtils.error_Message(client, '***Usage:*** ```>clear <amount>```') ; return
+                else:
+                    await client.channel.purge(limit=int(amount))
+                    await client.channel.send('cleared {} messages'.format(amount))
+    except:
+        await discordUtils.error_Message(client, "***Don't forget to use a number between 1-100!***")
