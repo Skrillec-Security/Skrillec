a7ee2ae4a0bd58cc511b6016412b4a2b7d4c6417
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-19 18:10:30 -0500

    Create command_handler.py

---
+++ src/bot/command_handler.py
@@ -0,0 +1,22 @@
+import os, sys, time
+
+from ..commands.help import *
+from ..commands.clear import *
+from ..commands.help_mod import *
+from ..commands.stress import *
+
+async def handle_cmd(client, msg: str, cmd: str, args):
+    # Help Commands
+    if cmd == "help": await help(client)
+    elif cmd == "help" and rgs[1] == "mod": await help_mod()
+
+    ## Moderation Commands
+    elif cmd == "clear": await clear(client)
+    elif cmd == "purge": await purge(client)
+    # elif cmd == "ban": 
+    # elif cmd == "kick":
+    # elif cmd == "lock_channel":
+    # elif cmd == "lock_server":
+
+    ## DDOS Commands
+    elif cmd == "stress": await stress(client, msg, cmd, args)
