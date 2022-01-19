9c0926f70bea0bb4bd518df43888c0466fa51f37
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-19 18:11:55 -0500

    Create custom_utils.py

---
+++ src/utils/custom_utils.py
@@ -0,0 +1,16 @@
+import os, sys, time
+
+def ipv4_check(ip: str):
+    try:
+        if len(ip) <= len("255.255.255.255"):
+            for i in range(0, 4):
+                if ip.contains("."): continue
+                else: return 0
+            ip_args = ip.split(".")
+            if ip_args[0].int() > 255: return 0
+            if ip_args[1].int() > 255: return 0
+            if ip_args[2].int() > 255: return 0
+            if ip_args[3].int() > 255: return 0
+    except:
+        return 0
+    return 1
