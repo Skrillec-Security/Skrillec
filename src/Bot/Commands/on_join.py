3a10ba15a12bdcdcd2ccc0f3d6465f73f8590722
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-19 15:51:13 -0500

    Delete on_join.py

--- src/Bot/Commands/on_join.py
+++
@@ -1,9 +0,0 @@
-import discord
-
-async def on_Join(client):
-    role = 'Members'
-
-
-    add_Roles = discord.utils.get(client.guild.roles, name=role)
-    await client.author.add_roles(add_Roles)
-    print('{} was given {} role!'.format(client.author, add_Roles))
