18a3c20bf84ed615503f2ed4ba7c444b638ba0ea
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-19 18:10:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-import discord
+import os, sys, time, requests, discord
+
+# Importing submodules
+from src.bot.command_handler import *
+from src.config.main import *
+from src.config.config_cmds import *
+from src.utils.embed_msg import *
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+    async def on_message(self, client):
+        if (client.content).startswith(Config.bot_prefix):
+            ## Look for command in 'src/commands/' folder!
+            full_cmd = client.content
+            cmd_args = (client.content).split(" ")
+            cmd = (cmd_args[0])[1:]
+            print(cmd)
+
+            all_cmds = get_cmds()
+            if cmd in all_cmds:
+                await handle_cmd(client, full_cmd, cmd, cmd_args)
+            else:
+                await embed(client, "Error", "Command not found!")
+
+
+
+        print('client from {0.author}: {0.content}'.format(client))
 
 client = MyClient()
 client.run('OTMxOTQ4MTUwNzk5ODcyMDQw.YeL2WQ.mRzC6co3Yn0YPMJOGdojLlUTZrc')
