dc598d505a1ff1571a22d5b23cec49ef4fee69a5
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-19 15:50:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import os, sys, time, threading
-from src.Bot.main import *
-from src.Config.main import *
+import discord
 
-bot = Skrillec()
-threading.Thread(target=bot.run, args=('OTA5NTAwOTkwMTM3NDQyNDE2.YZFMzQ.MGAdb6rsyU1OOGFrOAgIYLKCi_M',)).start()
-while(1):
-    input_cmd = input(">>> ")
-    if input_cmd == "c":
-        exit(0)
-    
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('OTMxOTQ4MTUwNzk5ODcyMDQw.YeL2WQ.mRzC6co3Yn0YPMJOGdojLlUTZrc')
