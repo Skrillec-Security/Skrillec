7a34def38ce5db45543ed18a58e2eb0f50739329
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-19 19:38:55 -0500

    Update crud.py

--- src/authentication/crud.py
+++ src/authentication/crud.py
@@ -6,16 +6,16 @@ def parse_dbLine(line):
     return ((line.replace("('", "")).replace("')", "")).split("','")
 
 def find_user(userid):
-    # try:
-    users = open(Config.local_db_path, "r").read()
-    for user in users.split("\n"):
-        if len(user) == 0: return 0
-        info = parse_dbLine(user)
-        print("DB ID Pulled: " + info[1] + " | " + str(userid))
-        if info[1] == str(userid):
-            return info
-    # except:
-    #     return 0
+    try:
+        users = open(Config.local_db_path, "r").read()
+        for user in users.split("\n"):
+            if len(user) == 0: return 0
+            info = parse_dbLine(user)
+            # print("DB ID Pulled: " + info[1] + " | " + str(userid)) ##debug test
+            if info[1] == str(userid):
+                return info
+    except:
+        return 0
     return 0
 
 def register_user(discord_name, discord_id):
