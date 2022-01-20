86fb7963f4af7851fdcc70358e78f7a1c74ba139
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-19 19:55:11 -0500

    Create purge.py

---
+++ src/commands/purge.py
@@ -0,0 +1,12 @@
+import os, sys, time
+
+from ..utils.embed_msg import *
+from ..config.main import *
+
+async def purge(client, args):
+    try:
+        int(args[1])
+    except:
+        return await embed(client, "Error", "You did not provide a valid number!")
+    if int(args[1]) > 500: return await embed(client, "Error", "You provided a number over the maximum of messages i can delete!. Use " + Config.bot_prefix + "clear to completely clear the channel!")
+    await client.channel.purge(limit=int(args[1]))
