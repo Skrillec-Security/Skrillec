038f89861822b7e2ee5431dc88205919a7c7e1c4
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-19 19:39:11 -0500

    Delete stress.py

--- src/commands/stress.py
+++
@@ -1,7 +0,0 @@
-import os, sys, time
-
-from ..config.main import *
-
-async def stress(client, fmsg, cmd, args):
-    if len(args) < 4: return (await client.channel.send("[x] Error, Invalid argument\nUsage {}stress <ip> <port> <time> <method>\nExample: {}stress 1.1.1.1 80 300 UDP".format(Config.bot_prefix, Config.bot_prefix)))
-    await client.channel.send("works")
