a3a9100d7e455aac82ac6749c7eec765bc3a5b01
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-20 19:23:22 -0500

    Update command_handler.py

--- src/bot/command_handler.py
+++ src/bot/command_handler.py
@@ -5,19 +5,23 @@ from ..commands.clear import *
 from ..commands.help_mod import *
 from ..commands.attack import *
 from ..commands.purge import *
+from ..commands.role import *
+from ..commands.attack import *
 
 async def handle_cmd(client, msg: str, cmd: str, args):
     # Help Commands
-    if cmd == "help": await help(client)
-    elif cmd == "help" and rgs[1] == "mod": await help_mod()
+    if cmd == "help" and len(msg) == 5: await help(client)
+    elif cmd == "help" and args[1] == "mod": await help_mod(client)
 
     ## Moderation Commands
     elif cmd == "clear": await clear(client)
     elif cmd == "purge": await purge(client, args)
+    elif cmd == "role": await role(client, args)
     # elif cmd == "ban": 
     # elif cmd == "kick":
     # elif cmd == "lock_channel":
     # elif cmd == "lock_server":
 
     ## DDOS Commands
+    elif cmd == "help" and args[1] == "attack": await help_attack(client, args)
     elif cmd == "attack": await attack(client, msg, cmd, args)
