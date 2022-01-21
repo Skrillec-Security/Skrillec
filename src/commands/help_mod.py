3fe287e6ea3bcc94982bca293211ed3b28dd2e8e
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-20 19:23:56 -0500

    Update help_mod.py

--- src/commands/help_mod.py
+++ src/commands/help_mod.py
@@ -1,4 +1,13 @@
 import os, sys, time
 
+from ..discord_utils.embed_msg import *
+
 async def help_mod(client):
-    await client.channel.send("Working")
+    mod_list = """```  Usage       Tools
+____________________________________
+  purge       Delete an amount of msgs
+  clear       Clear's Channel
+  kick        Kick User
+  ban         Ban User
+  unban       Unban User```"""
+    await embed(client, "Moderation Commands", mod_list)
