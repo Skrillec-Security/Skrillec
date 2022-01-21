1b7efdbc922c06887f604598eb25a9113fec70c9
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-21 07:35:36 -0500

    Update log_system.py

--- src/utils/log_system.py
+++ src/utils/log_system.py
@@ -1,12 +1,24 @@
 import os, sys, time
 
 from ..config.main import *
+## discord_name, discord_id, server_name, server_id, channel_name, channel_id
+class Logger:
+    global discord_name
+    global discord_id
+    global server_name
+    global server_id
+    global channel_name
+    global channel_id
 
-class Logger: 
-    def new_log(discord_name, discord_id, args):
-        ## Current Time
-        log_formart = """
-[User]: 
-        """
-        cmd_db = open(Config.local_cmdlog_db_path, "a")
-        cmd_db.write("")
+    global log_formatted
+    def set_info(dname, did, sname, sid, cname, cid):
+        discord_name = dname
+        discord_id = did
+        server_name = sname
+        server_id = sid
+        channel_name = cname
+        channel_id = sid
+        create_format():
+        
+    def create_format():
+        log_format = """
