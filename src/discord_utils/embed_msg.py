436662ee1a9bbc4e096189c3f89abc7b889ab0bf
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-01-23 16:14:14 -0500

    Update embed_msg.py

--- src/discord_utils/embed_msg.py
+++ src/discord_utils/embed_msg.py
@@ -1,7 +1,13 @@
-import os, sys, time, discord
+import os, sys, time, discord, random
 
 async def embed(client, ttitle, ddescription):
     embedVar = discord.Embed(title=ttitle, description=ddescription, color=0xff0000)
+    lul = ['https://c.tenor.com/ccKhodmHDUEAAAAC/la-capone.gif',
+    'https://media1.giphy.com/media/11Ad7FqUiNMRAA/giphy.gif',
+    'https://i.imgur.com/S1yXrH8.gif',
+    'https://c.tenor.com/ofYCY_OJQ1kAAAAd/hacker-hack.gif',
+    'https://media3.giphy.com/media/YQitE4YNQNahy/giphy-downsized-large.gif']
+    embedVar.set_image(url=lul[random.randrange(0, len(lul))])
     # embedVar.add_field(name="Field1", value="hi", inline=False)
     # embedVar.add_field(name="Field2", value="hi2", inline=False)
     await client.channel.send(embed=embedVar)
