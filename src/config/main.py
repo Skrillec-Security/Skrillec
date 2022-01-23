d0fa4bc95ec6d21a0dccf37b95f4df7b2c5cfa4b
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-23 17:56:36 -0500

    Update main.py

--- src/config/main.py
+++ src/config/main.py
@@ -5,14 +5,20 @@ from ..commands.clear import *
 
 class Config:
     ## Windows paths
-    local_db_path_backup = os.getcwd() + "\\src\\db\\users.skrillec"
+    local_db_path_backup = os.getcwd() + "\\src\\assets\\db\\users.skrillec"
+    local_attacks_path_backup = os.getcwd() + "\\src\\assets\\logs\\attacks.log" #attack logs
+    local_cmdlog_db_path_backup = os.getcwd() + "\\src\\logs\\cmds.log" # cmds logs
+    local_login_path_backup = os.getcwd() + "\\src\\assets\\logs\\logins.log" #login logs
+    local_admin_path_backup = os.getcwd() + "\\src\\assets\\logs\\admin_cmds.log" # admin cmds logs
     local_settings_path_backup = os.getcwd() + "\\src\\assets\\settings.skrillec"
 
 
 
     ## Linux Paths
-    local_db_path = os.getcwd() + "/src/db/users.skrillec"
-    local_api_path = os.getcwd() + "/src/db/apis.skrillec"
-    local_cmdlog_db_path = os.getcwd() + "/src/logs/command.log"
-    local_settings_path = os.getcwd() + "/src/settings.skrillec"
+    local_db_path = os.getcwd() + "/src/assets/db/users.skrillec" #db
+    local_attacks_path = os.getcwd() + "/src/assets/logs/attacks.log" #attack logs
+    local_cmdlog_db_path = os.getcwd() + "/src/logs/cmds.log" # cmds logs
+    local_login_path = os.getcwd() + "/src/assets/logs/logins.log" #login logs
+    local_admin_path = os.getcwd() + "/src/assets/logs/admin_cmds.log" # admin cmds logs
+    local_settings_path = os.getcwd() + "/src/assets/settings.skrillec"
     bot_prefix = ";"
