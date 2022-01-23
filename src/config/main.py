bc475c6c0f92995358131252fcf7d02437cf4d9d
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-01-23 16:13:49 -0500

    Update main.py

--- src/config/main.py
+++ src/config/main.py
@@ -4,8 +4,9 @@ from ..commands.help import *
 from ..commands.clear import *
 
 class Config:
-    local_db_path = os.getcwd() + "/src/assets/db/users.skrillec"
-    local_api_path = os.getcwd() + "/src/assets/db/apis.skrillec"
-    local_cmdlog_db_path = os.getcwd() + "/src/assets/logs/command.log"
-    local_settings_path = os.getcwd() + "/src/assets/settings.skrillec"
+    local_db_path_backup = os.getcwd() + "\\src\\db\\users.skrillec"
+    local_db_path = os.getcwd() + "/src/db/users.skrillec"
+    local_api_path = os.getcwd() + "/src/db/apis.skrillec"
+    local_cmdlog_db_path = os.getcwd() + "/src/logs/command.log"
+    local_settings_path = os.getcwd() + "/src/settings.skrillec"
     bot_prefix = ";"
