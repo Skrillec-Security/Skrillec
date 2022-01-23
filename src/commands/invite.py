5032154c2aa2a86f0a022019645634f48a7ff4c9
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-23 18:39:40 -0500

    Create invite.py

---
+++ src/commands/invite.py
@@ -0,0 +1,6 @@
+import os, sys, time
+
+from ..discord_utils.embed_msg import *
+
+async def invite_me(client):
+    await embed(client, "Invite me", "Invite me to your server!\n\n```https://discord.com/api/oauth2/authorize?client_id=931948150799872040&permissions=8&scope=bot```")
