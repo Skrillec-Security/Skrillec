07f4f5c8a373b149cb4e004597213edb3b51d413
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-23 17:26:24 -0500

    Update main.py

--- src/config/main.py
+++ src/config/main.py
@@ -4,7 +4,13 @@ from ..commands.help import *
 from ..commands.clear import *
 
 class Config:
+    ## Windows paths
     local_db_path_backup = os.getcwd() + "\\src\\db\\users.skrillec"
+    local_settings_path_backup = os.getcwd() + "\\src\\assets\\settings.skrillec"
+
+
+
+    ## Linux Paths
     local_db_path = os.getcwd() + "/src/db/users.skrillec"
     local_api_path = os.getcwd() + "/src/db/apis.skrillec"
     local_cmdlog_db_path = os.getcwd() + "/src/logs/command.log"
