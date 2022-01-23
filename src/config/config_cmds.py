3143cb18d3f4dcdabfd90a82839798b5f588aa2b
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-23 18:54:59 -0500

    Update config_cmds.py

--- src/config/config_cmds.py
+++ src/config/config_cmds.py
@@ -2,6 +2,7 @@ import os, sys, time
 
 def get_cmds():
     all_cmds = os.listdir(os.getcwd() + "/src/commands/")
+    all_cmds += os.listdir(os.getcwd() + "/src/commands/ansi_cmds")
     new_list = ["", ""]
     for i in all_cmds:
         new_list.append(i.replace(".py", ""))
