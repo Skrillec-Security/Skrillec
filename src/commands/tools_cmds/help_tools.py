125a5edf30468eebf473da08be0e800ffd9626b0
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-25 17:53:56 -0500

    Create help_tools.py

---
+++ src/commands/tools_cmds/help_tools.py
@@ -0,0 +1,19 @@
+import sys, os, time
+
+from ...discord_utils.embed_msg import *
+
+async def help_tools(client):
+    help_cmds = """```  Usage       Tools
+____________________________________
+  ping        Ping an IP/URL
+  geoip       IP Lookup
+  pscan       Port Scanner
+  url         Gets IP of URL
+  ip2url      Find URL to IP
+  whosis      Whois Checker
+  urlr        Advanced DNS Checker
+  skype       Skype Resolver
+  phone       Phone Lookup
+  
+  More tools coming soon!```"""
+    await embed(client, "Tools Commands", help_cmds)
