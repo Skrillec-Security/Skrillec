dfa6c2a025a7f9c4f0d69891d859fbe1a0b8d6a0
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-26 17:31:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ from src.config.config_cmds import *
 from src.config.configure import *
 from src.discord_utils.embed_msg import *
 from src.utils.custom_utils import *
+from src.blacklist.main import *
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+
+    async def on_join(self, client):
+        if client.author.id in BlacklistedUsers():
+            await client.create_dm.send("You're a broke fucking skid. Stop trying to join random")
+            await client.delete()
+            await client.kick()
 
     async def on_message(self, client):
         if url_block(client.content) == 0 | client.author.id != "918258241576792115":
@@ -34,4 +41,4 @@ class MyClient(discord.Client):
 
 client = MyClient()
 python_is_gay_as_fuck = str(Configuration.get_token())
-client.run('OTMxOTQ4MTUwNzk5ODcyMDQw.YeL2WQ.QvU6fe23m0tU5tFAd72RyO6TnGM')
+client.run('OTMxOTQ4MTUwNzk5ODcyMDQw.YeL2WQ.dgL122UDEptrILE73A-K_BQomLk')
