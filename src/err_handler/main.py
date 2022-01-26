e28f3e99627b83109a1dcacc063249cbbe773824
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-26 14:48:56 -0500

    Create main.py

---
+++ src/err_handler/main.py
@@ -0,0 +1,25 @@
+import os, sys, time, requests
+
+class ErrHandler:
+    global err_msg
+    global err_type
+    err_types = ['API', 'DB Not Found', 'Setting file not found']
+    def log_err(err_t, err_m):
+        err_type = err_t
+        err_msg = err_m
+        make_log_format()
+
+    def make_log_format():
+        output = """
+        """
+
+
+    def log_to_file(new_log):
+        try:
+            log_db = open(local_settings_path, "a")
+            log_db.write(new_log)
+            log_db.close()
+            return 1
+        except:
+            return -1
+        return 0
