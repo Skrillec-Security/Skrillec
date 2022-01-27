5b8293636e9d22140b63c851c7c4938933fcc090
Beta security <54654703+BetaCyberSec@users.noreply.github.com>
2022-01-27 13:14:22 -0500

    Update clock.py

--- src/commands/mod_cmds/clock.py
+++ src/commands/mod_cmds/clock.py
@@ -1,3 +1,14 @@
-"""
-    clock.py => Channel Lock
-"""
+import os, sys, time, discord
+
+from ...discord_utils.embed_msg import *
+
+async def clock(client):
+    await client.channel.send("Locking Channel")
+
+async def create_lcok_role(ctx, *, ChannelLock):
+	guild = ctx.guild
+	await guild.create_role(name=ChannelLock)
+
+'''
+not done yet
+'''
