86e5ee5569ead10f660bec9994ead0f64dccb974
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-01-28 17:00:47 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,43 @@
-import os, sys, time
+import os, sys, time, subprocess, test
 
-from src.commands.ansi_cmds.gradient import *
+class GetCurrent:
+    old_rx = 0
+    old_tx = 0
+    new_rx = 0
+    new_tx = 0
+    final_rx = 0
+    final_tx = 0
+    pps = 0
+    def set_old(rx, tx):
+        GetCurrent.old_rx = rx
+        GetCurrent.old_tx = tx
 
-print(gradient([255, 0, 0], [0, 0, 255], "testing this"))
+    def set_new(rx, tx):
+        GetCurrent.new_rx = rx
+        GetCurrent.new_tx = tx
+
+    def get_pps():
+        GetCurrent.final_rx = GetCurrent.old_rx - GetCurrent.new_rx
+        GetCurrent.final_tx = GetCurrent.old_tx - GetCurrent.new_tx
+        GetCurrent.pps = GetCurrent.final_tx - GetCurrent.final_rx
+
+    def Interface():
+        WiFi_Interface = subprocess.getoutput("ifconfig").split()[0]
+        WiFi_Interface = WiFi_Interface.strip().replace(":", "")
+        return WiFi_Interface
+    
+    def tx_packets():
+        Interface = GetCurrent.Interface()
+        return (subprocess.getoutput(f"cat /sys/class/net/{str(Interface)}/statistics/tx_packets"))
+    
+    def rx_packets():
+        Interface = GetCurrent.Interface()
+        return (subprocess.getoutput(f"cat /sys/class/net/{str(Interface)}/statistics/rx_packets"))
+
+    def run():
+        GetCurrent.set_old(GetCurrent.rx_packets(), GetCurrent.tx_packets())
+        time.sleep(1)
+        GetCurrent.set_new(GetCurrent.rx_packets(), GetCurrent.tx_packets())
+        current_pps = GetCurrent.get_pps()
+        println(current_pps)
+
