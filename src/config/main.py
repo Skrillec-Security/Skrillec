fee149db8e5422f518c4a3dce6f3685c5ef2e6ac
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-01-29 17:13:48 -0500

    Update main.py

--- src/config/main.py
+++ src/config/main.py
@@ -3,24 +3,24 @@ import os, sys, time
 
 class Config:
     ## Windows paths
-    local_db_path_backup = os.getcwd() + "\\src\\assets\\db\\users.skrillec"
-    local_attacks_path_backup = os.getcwd() + "\\src\\assets\\logs\\attacks.log" #attack logs
-    local_cmdlog_db_path_backup = os.getcwd() + "\\src\\logs\\cmds.log" # cmds logs
-    local_login_path_backup = os.getcwd() + "\\src\\assets\\logs\\logins.log" #login logs
-    local_admin_path_backup = os.getcwd() + "\\src\\assets\\logs\\admin_cmds.log" # admin cmds logs
-    local_settings_path_backup = os.getcwd() + "\\src\\assets\\settings.skrillec"
-    local_err_path = os.getcwd() + "\\src\\assets\\logs\\erros.log"
-    local_blacklist_path = os.getcwd() + "\\src\\assets\\db\\blacklisted.skrillec"
+    local_db_path_backup =          os.getcwd() + "\\src\\assets\\db\\users.skrillec"
+    local_attacks_path_backup =     os.getcwd() + "\\src\\assets\\logs\\attacks.log" #attack logs
+    local_cmdlog_db_path_backup =   os.getcwd() + "\\src\\assets\\logs\\cmds.log" # cmds logs
+    local_login_path_backup =       os.getcwd() + "\\src\\assets\\logs\\logins.log" #login logs
+    local_admin_path_backup =       os.getcwd() + "\\src\\assets\\logs\\admin_cmds.log" # admin cmds logs
+    local_settings_path_backup =    os.getcwd() + "\\src\\assets\\settings.skrillec"
+    local_err_path_backup =         os.getcwd() + "\\src\\assets\\logs\\erros.log"
+    local_blacklist_path_backup =   os.getcwd() + "\\src\\assets\\db\\blacklisted.skrillec"
 
 
 
     ## Linux Paths
-    local_db_path = os.getcwd() + "/src/assets/db/users.skrillec" #db
-    local_attacks_path = os.getcwd() + "/src/assets/logs/attacks.log" #attack logs
-    local_cmdlog_db_path = os.getcwd() + "/src/logs/cmds.log" # cmds logs
-    local_login_path = os.getcwd() + "/src/assets/logs/logins.log" #login logs
-    local_admin_path = os.getcwd() + "/src/assets/logs/admin_cmds.log" # admin cmds logs
-    local_settings_path = os.getcwd() + "/src/assets/settings.skrillec"
-    local_err_path = os.getcwd() + "/src/assets/logs/erros.log"
-    local_blacklist_path = os.getcwd() + "/src/assets/db/blacklisted.skrillec"
+    local_db_path =                 os.getcwd() + "/src/assets/db/users.skrillec" #db
+    local_attacks_path =            os.getcwd() + "/src/assets/logs/attacks.log" #attack logs
+    local_cmdlog_db_path =          os.getcwd() + "/src/logs/cmds.log" # cmds logs
+    local_login_path =              os.getcwd() + "/src/assets/logs/logins.log" #login logs
+    local_admin_path =              os.getcwd() + "/src/assets/logs/admin_cmds.log" # admin cmds logs
+    local_settings_path =           os.getcwd() + "/src/assets/settings.skrillec"
+    local_err_path =                os.getcwd() + "/src/assets/logs/erros.log"
+    local_blacklist_path =          os.getcwd() + "/src/assets/db/blacklisted.skrillec"
     bot_prefix = ";"
