b3c580e3fadba4ebff27bef727c387af5a8da76a
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-01-29 17:13:53 -0500

    Update pscan.py

--- src/commands/tools_cmds/pscan.py
+++ src/commands/tools_cmds/pscan.py
@@ -1,2 +1,8 @@
-import os, sys, time
+import os, sys, time, requests
 
+from ...discord_utils.embed_msg import *
+
+async def PortScan(client, args):
+    if len(args) < 1: return (await embed(client, "Error", "[x] Error, Invalid argument\nUsage: ;pscan <ip>\nExample: ;psan 1.1.1.1"))
+    ports = requests.get("https://api.skrillec.pw/pscan.php?q={0}".format(args[1])).text
+    await embed(client, "Port Scan Results", "```" + ports.replace("</br>", "") + "```")
