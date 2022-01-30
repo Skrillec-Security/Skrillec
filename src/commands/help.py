8a27720d7c3a0d0a8c3f5b1588c37ff249068181
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-30 18:11:46 -0500

    Update help.py

--- src/commands/help.py
+++ src/commands/help.py
@@ -3,11 +3,4 @@ import os, sys, time, discord
 from ..discord_utils.embed_msg import *
 
 async def help(client):
-    help_list = """```  Usage       Tools
-____________________________________
-  help        All help list
-  help mod    Moderation Commands
-  help tools  IP/URL Network Tools
-  help attack Stresser (Premium)
-  help ansi   ANSI/ASCII Tools```"""
-    await embed(client, "Help Commands", help_list)
+  await EmbedMessages.Help_Menu(client)
