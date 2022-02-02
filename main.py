3b24a29dd55f3f988712217cc21e722e4db014e3
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-02-02 15:25:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -43,4 +43,4 @@ class MyClient(discord.Client):
 
 client = MyClient()
 python_is_gay_as_fuck = str(Configuration.get_token())
-client.run('OTMxOTQ4MTUwNzk5ODcyMDQw.YeL2WQ.WOPgYUeElY3ivjkZU9rCPUW_srE')
+client.run('OTMxOTQ4MTUwNzk5ODcyMDQw.YeL2WQ.TOnT4KMfZ2-jxlHL34wtn2xRKbs')
