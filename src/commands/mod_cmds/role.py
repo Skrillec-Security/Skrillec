8b8e120228ca54cecf69ce8684a81081ffe27c7a
Lamp07 <96823938+Lamp07@users.noreply.github.com>
2022-02-10 22:40:46 -0800

    Update role.py

--- src/commands/mod_cmds/role.py
+++ src/commands/mod_cmds/role.py
@@ -1,6 +1,11 @@
-import os, sys, time
+from dis import disco
+import os, sys, time, discord
 
 from ...discord_utils.embed_msg import *
 
-async def Role(client, args):
-    return await embed(client, "Role", "Role set on ")
+async def Role(client, role: discord.Role, member: discord.Member = None):
+    return await embed(client, "Role", "Role set on {}".format(member))
+
+"""
+not done yet
+"""
