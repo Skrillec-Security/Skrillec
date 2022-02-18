644a18b3a9ed6458424dbaddd3180030953f0dee
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-02-17 20:35:03 -0500

    Update invite.py

--- src/commands/invite.py
+++ src/commands/invite.py
@@ -3,4 +3,4 @@ import os, sys, time
 from ..discord_utils.embed_msg import *
 
 async def invite_me(client):
-    await embed(client, "Invite me", "Invite me to your server!\n\n```https://discord.com/api/oauth2/authorize?client_id=931948150799872040&permissions=8&scope=bot```")
+    await embed(client, "Invite me", "Invite me to your server!\n\n```https://discord.com/api/oauth2/authorize?client_id=943994801752244334&permissions=8&scope=bot```")
