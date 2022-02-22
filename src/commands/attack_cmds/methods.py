d14bdc28e1cd405128d9198f152f1cc67adb8c71
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-02-22 10:45:33 -0800

    Update methods.py

--- src/commands/attack_cmds/methods.py
+++ src/commands/attack_cmds/methods.py
@@ -2,32 +2,36 @@ import os, sys, time
 from ...discord_utils.embed_msg import *
 
 async def methods(client):
-    methods_list = """• home
-• dvr
-• openvpn2
-• wsd
-• snmp
-• ovh-gb
-• ovh-drown
-• ovh-acid
-• ovh-udp
-• 100up-acid
-• cpu
-• 22-ssh
-• nfo-acid
-• nfo-ssh
-• syn-star
-• ack-star
-• tempest-acid
-• psn-rape
-• server-uwsi
-• udpbypass-v2
-• udp-v1
-• vpn-mix-v3
-• ark-255
-• ark-lift
-• rust-game
-• fivem-bypass
-• syn-vr
-• std-vr"""
-    await embed(client, "Methods", methods_list)
+    methods_list = """LKZ-GET
+LKZ-POST
+LKZ-HEAD
+LKZ-FIVEM
+LKZ-TCPBYPASS
+LKZ-NTP
+LKZ-OVHBETA
+LKZ-KILLALL
+LKZ-ICMP
+LKZ-HOME
+LKZ-VSE
+LKZ-GAMEV2
+LKZ-SYNBYPASS
+LKZ-UDPBYPASS
+LKZ-DNS
+LKZ-NFO
+LKZ-FIVEMTCP
+LKZ-OVHBETAV2
+LKZ-TCPEXTRA
+LKZ-OVHV4"""
+    methods_list2 = """HOME-TRC
+TCP-TRC
+SSH-TRC
+NFO-TRC
+OVH-TRC
+GAME-TRC
+FIVEM-TRC
+ARK-TRC
+RUST-TRC
+MC-TRC
+HTTPS-TRC"""
+    await embed(client, "API 1 Methods", "```{0}```".format(methods_list))
+    await embed(client, "API 2 Methods", "```{0}```".format(methods_list2))
