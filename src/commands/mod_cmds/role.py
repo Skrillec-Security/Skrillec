16007003bf45134a351336ab48be7169d7361506
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-02-22 10:45:42 -0800

    Update role.py

--- src/commands/mod_cmds/role.py
+++ src/commands/mod_cmds/role.py
@@ -3,8 +3,18 @@ import os, sys, time, discord
 
 from ...discord_utils.embed_msg import *
 
-async def Role(client, role: discord.Role, member: discord.Member = None):
-    return await embed(client, "Role", "Role set on {}".format(member))
+async def Role(client, args):
+    if len(args) < 1:
+        return (await embed(client, "Error", "Invalid argument\nUsage: {}role <userid/@tag> <role/@tag>"))
+
+    userid = args[1]
+    role = args[2]
+    userid = userid.replace("<@", "").replace(">","").replace("!","")
+    role = role.replace("<@", "").replace(">", "").replace("&", "")
+    user = await client.guild.fetch_member(userid)
+    role = discord.utils.get(client.guild.roles, name=str(role))
+    await user.add_roles(role)
+    return await embed(client, "Role", "Role set on <@{}>".format(userid))
 
 """
 not done yet
