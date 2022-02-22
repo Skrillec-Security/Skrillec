a20f7841f9c0a2869769b0b3d9a7ba801ac1755b
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-02-22 10:45:45 -0800

    Update embed_msg.py

--- src/discord_utils/embed_msg.py
+++ src/discord_utils/embed_msg.py
@@ -4,11 +4,11 @@ from .embed_msg import *
 
 async def embed(client, ttitle, ddescription):
     embedVar = discord.Embed(title=ttitle, description=ddescription, color=0xff0000)
-    lul = ['https://media1.giphy.com/media/11Ad7FqUiNMRAA/giphy.gif',
-    'https://i.imgur.com/S1yXrH8.gif',
-    'https://c.tenor.com/ofYCY_OJQ1kAAAAd/hacker-hack.gif',
-    'https://media3.giphy.com/media/YQitE4YNQNahy/giphy-downsized-large.gif']
-    embedVar.set_image(url=lul[random.randrange(0, len(lul))])
+    # lul = ['https://media1.giphy.com/media/11Ad7FqUiNMRAA/giphy.gif',
+    # 'https://i.imgur.com/S1yXrH8.gif',
+    # 'https://c.tenor.com/ofYCY_OJQ1kAAAAd/hacker-hack.gif',
+    # 'https://media3.giphy.com/media/YQitE4YNQNahy/giphy-downsized-large.gif']
+    # embedVar.set_image(url=lul[random.randrange(0, len(lul))])
     # embedVar.add_field(name="Field1", value="hi", inline=False)
     # embedVar.add_field(name="Field2", value="hi2", inline=False)
     await client.channel.send(embed=embedVar)
