efed119e9222618c367831cc17f0c0a8e8689b22
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-02-22 16:21:44 -0800

    Update register.py

--- src/commands/account_cmds/register.py
+++ src/commands/account_cmds/register.py
@@ -6,5 +6,5 @@ from ...discord_utils.embed_msg import *
 async def Register(client):
     username = client.author.name
     user_id = client.author.id
-    Crud.register_user(username, user_id)
+    Crud.register_user(str(username), str(user_id))
     await embed(client, "successfully registered!", "Username: {0}\nUser ID     : {1}".format(username, user_id))
