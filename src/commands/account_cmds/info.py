497249029a9e1bcabaffa1a974b30e48e966fbaa
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-02-22 16:21:42 -0800

    Update info.py

--- src/commands/account_cmds/info.py
+++ src/commands/account_cmds/info.py
@@ -5,11 +5,9 @@ from ...discord_utils.embed_msg import *
 
 async def Info(client):
     info = Crud.find_user(str(client.author.id))
-    output = """```[User]: {0}, {1}
-[Level]: {2}
-[Max time]: {3}
-[Concurrent]: {4}
-[Ongoing]: {5}
-[Admin]: {6}
-[Expiry]: {7}```""".format(info[0], info[1], info[2], info[3], info[4], info[5], info[6], info[7])
-    await embed(client, "User Information", output)
+    try:
+        if info == 0 | info == -1:
+            return (await embed(client, "Info Error", "You are not registered. Type `{}register` in order to register!"))
+    except:
+        output = "```[User]: {0}, {1}\n[Level]: {2}\n[Max time]: {3}\n[Concurrent]: {4}\n[Ongoing]: {5}\n[Admin]: {6}\n[Expiry]: {7}```".format(str(info[0]), str(info[1]), str(info[2]), str(info[3]), str(info[4]), str(info[5]), str(info[6]), str(info[7]))
+    return await embed(client, "User Information", output)
