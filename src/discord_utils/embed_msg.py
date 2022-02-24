3863d298628e4517a49f5f70f442f5d242999e88
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-02-24 10:33:12 -0800

    Update embed_msg.py

--- src/discord_utils/embed_msg.py
+++ src/discord_utils/embed_msg.py
@@ -15,15 +15,15 @@ async def embed(client, ttitle, ddescription):
 
 
 class EmbedMessages:
-    lul = ['https://c.tenor.com/m-efsi4uFsoAAAAC/lil-herb-g-herbo.gif', 
-    'https://media1.giphy.com/media/11Ad7FqUiNMRAA/giphy.gif',
-    'https://i.imgur.com/S1yXrH8.gif',
-    'https://c.tenor.com/ofYCY_OJQ1kAAAAd/hacker-hack.gif',
-    'https://media3.giphy.com/media/YQitE4YNQNahy/giphy-downsized-large.gif']
+    # lul = ['https://c.tenor.com/m-efsi4uFsoAAAAC/lil-herb-g-herbo.gif', 
+    # 'https://media1.giphy.com/media/11Ad7FqUiNMRAA/giphy.gif',
+    # 'https://i.imgur.com/S1yXrH8.gif',
+    # 'https://c.tenor.com/ofYCY_OJQ1kAAAAd/hacker-hack.gif',
+    # 'https://media3.giphy.com/media/YQitE4YNQNahy/giphy-downsized-large.gif']
 
     async def Help_Menu(client):
         embedVar = discord.Embed(title="Help Commands", description="List of commands", color=0xff0000)
-        embedVar.set_image(url=EmbedMessages.lul[random.randrange(0, len(EmbedMessages.lul))])
+        # embedVar.set_image(url=EmbedMessages.lul[random.randrange(0, len(EmbedMessages.lul))])
         embedVar.add_field(name="help", value="All help list", inline=False)
         embedVar.add_field(name="help acct", value="Account Commands", inline=False)
         embedVar.add_field(name="help mod", value="Moderation Commands", inline=False)
@@ -34,7 +34,7 @@ class EmbedMessages:
 
     async def Acct_Menu(client):
         embedVar = discord.Embed(title="Help Commands", description="List of commands", color=0xff0000)
-        embedVar.set_image(url=EmbedMessages.lul[random.randrange(0, len(EmbedMessages.lul))])
+        # embedVar.set_image(url=EmbedMessages.lul[random.randrange(0, len(EmbedMessages.lul))])
         embedVar.add_field(name="register", value="Register for Attack System", inline=False)
         embedVar.add_field(name="info", value="Account plan", inline=False)
         embedVar.add_field(name="redeem", value="Redeem a token", inline=False)
