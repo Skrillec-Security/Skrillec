977baa60103e20598cb2536a66e8570ed010208a
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-02-24 10:32:59 -0800

    Delete clock.py

--- src/commands/mod_cmds/clock.py
+++
@@ -1,9 +0,0 @@
-import os, sys, time, discord
-
-from ...discord_utils.embed_msg import *
-
-async def clock(client):
-	for ch in client.guild.channels:
-		await ch.set_permissions(client.guild.default_role,  read_messages=True, send_messages=False)
-	
-	await embed(client, "Successfuly lock channel!", "all channels have been locked!!")
