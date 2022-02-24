eba10a14da099859f6f43e35928bcf6fa94d5fa3
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-02-23 16:01:38 -0800

    Update info.py

--- src/commands/account_cmds/info.py
+++ src/commands/account_cmds/info.py
@@ -9,5 +9,6 @@ async def Info(client):
         if info == 0 | info == -1:
             return (await embed(client, "Info Error", "You are not registered. Type `{}register` in order to register!"))
     except:
-        output = "```[User]: {0}, {1}\n[Level]: {2}\n[Max time]: {3}\n[Concurrent]: {4}\n[Ongoing]: {5}\n[Admin]: {6}\n[Expiry]: {7}```".format(str(info[0]), str(info[1]), str(info[2]), str(info[3]), str(info[4]), str(info[5]), str(info[6]), str(info[7]))
+        return await embed(client, "User Information", "[x] Error, No username found!")
+    output = "```[User]: {0}, {1}\n[Level]: {2}\n[Max time]: {3}\n[Concurrent]: {4}\n[Ongoing]: {5}\n[Admin]: {6}\n[Expiry]: {7}```".format(str(info[0]), str(info[1]), str(info[2]), str(info[3]), str(info[4]), str(info[5]), str(info[6]), str(info[7]))
     return await embed(client, "User Information", output)
