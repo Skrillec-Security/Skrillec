b1081626dc310131885224e3537dfc1907ce33f9
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-02-26 15:08:13 -0800

    Update ch_lock.py

--- src/commands/mod_cmds/ch_lock.py
+++ src/commands/mod_cmds/ch_lock.py
@@ -4,6 +4,7 @@ from ...discord_utils.embed_msg import *
 
 async def clock(client):
 	for ch in client.guild.channels:
-		await ch.set_permissions(client.guild.default_role, send_messages=False)
+		if ch.id == client.guild.id:
+			await ch.set_permissions(client.guild.default_role, send_messages=False)
 
 	await embed(client, "Successfuly lock channel!", "all channels have been locked!!")
